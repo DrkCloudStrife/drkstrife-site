9d1df57aa0a963f7e6019b457fa94b9225dc5f3a
Eduardo Garibay <dark.cloud2007@gmail.com>
2014-04-16 22:22:34 -0500

    Adds home page and I18n support

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,10 @@ module DrkstrifeSite
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.filter_parameters += [:password]
+
+    config.assets.enabled = true
+
+    config.server_hostname = Socket.gethostname.downcase.hash
   end
 end
