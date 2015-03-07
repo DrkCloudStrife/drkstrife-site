2e002c8204a32147669aff50e8c6ac97c0aed153
Eduardo Garibay <dark.cloud2007@gmail.com>
2015-03-06 22:09:45 -0600

    Adds phone variant detection.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+### 0.0.2
+- Adds phone variant
+
 ### 0.0.1
 - Basic article viewer.
 - Adds CHANGELOG.md
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_locale
+  before_action :detect_variant
 
   helper_method :current_locale
   helper_method :not_found
@@ -23,6 +24,17 @@ private
     rescue Exception => e
       raise ActionController::RoutingError.new('Not Found')
     end
+  end
+
+  def detect_variant
+    request.variant = case request.user_agent
+      when (/iPhone/i)
+        :phone
+      when (/Android.*mobile|mobile.*Android/i)
+        :phone
+      when (/Windows Phone/i)
+        :phone
+      end
   end
 
 end
