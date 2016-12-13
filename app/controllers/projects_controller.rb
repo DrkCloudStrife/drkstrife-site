45230c8aa50e08404d6211ca48338ad3203b7637
Eduardo Garibay <dark.cloud2007@gmail.com>
2016-12-13 12:35:53 -0600

    WIP: cleans up projects routes to follow articles markup.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,8 @@
 class ProjectsController < ApplicationController
   active_tab :projects
 
+  def show
+    project = params[:project].gsub('-', '_')
+    @current_page = "projects/partials/#{project}"
+  end
 end
