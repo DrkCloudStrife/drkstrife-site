ad5122a72e5fb27dd1658ca415ddcba8e8e07386
Eduardo Garibay <dark.cloud2007@gmail.com>
2019-08-04 16:20:21 -0500

    Making changes under the hood
    * New article
    * Start using flex box
    * Add Vue to webpacker
    * Remove Google+ authorship link

---
+++ app/javascript/src/main.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="subject-header">
+    <h1 class="title">Welcome</h1>
+  </header>
+  <div id="main">
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+</style>
