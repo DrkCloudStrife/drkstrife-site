d9128aa78b8ce8316225bf1a7e046148a003f621
Eduardo Garibay <dark.cloud2007@gmail.com>
2021-04-20 23:05:17 -0500

    Adds edit page to the admin controller

--- test/controllers/admin/home_controller_test.rb
+++ test/controllers/admin/home_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
+require 'helpers/application_controller_test_helper'
 
-class Admin::HomeControllerTest < ActionController::TestCase
+class Admin::HomeControllerTest < ApplicationControllerTest
 
   test "#index" do
     get :index
@@ -15,6 +16,11 @@ class Admin::HomeControllerTest < ActionController::TestCase
     get :logout, session: { auth_session: 'admin' }
     assert_response :redirect
     assert_equal(session[:auth_session], 'logged_out')
+  end
+
+  test "#edit" do
+    get :edit, session: { auth_session: 'admin' }, params: { slug: 'foobar' }
+    assert_response :success
   end
 
 end
